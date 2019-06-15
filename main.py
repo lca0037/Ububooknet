f8a061a1751f0db635290a6f05ad158cbf95f2b2
Luis Miguel Cabrejas Arce <33413592+lca0037@users.noreply.github.com>
2019-06-15 13:18:31 +0200

    #

--- main.py
+++ main.py
@@ -4,7 +4,9 @@
 #from tst import TestUnitarios
 from src import Controlador
 import unittest
+import os
 
 #runner = unittest.TextTestRunner()
 #result = runner.run(unittest.makeSuite(testUnitarios.testUnitarios))
-Controlador.app.run(threaded=True)
+port = int(os.environ.get('PORT', 5000))
+Controlador.app.run(threaded=True, host='0.0.0.0', port=port)
